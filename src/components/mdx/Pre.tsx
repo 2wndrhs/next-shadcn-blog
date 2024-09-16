e2f6360046dd1cd119af1ed07393b1f157e4fb90
2wndrhs <2wndrhs@gmail.com>
2024-09-16 11:50:57 +0900

    feat: add copy button to code block

---
+++ src/components/mdx/Pre.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { Check, Clipboard } from 'lucide-react';
+import { DetailedHTMLProps, HTMLAttributes, useRef, useState } from 'react';
+
+export default function Pre({
+  children,
+  ...props
+}: DetailedHTMLProps<HTMLAttributes<HTMLPreElement>, HTMLPreElement>) {
+  const [isCopied, setIsCopied] = useState(false);
+  const preRef = useRef<HTMLPreElement>(null);
+
+  const handleClickCopy = async () => {
+    const code = preRef.current?.textContent;
+
+    if (code) {
+      await navigator.clipboard.writeText(code);
+      setIsCopied(true);
+
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 3000);
+    }
+  };
+
+  return (
+    <pre ref={preRef} {...props} className='relative'>
+      <button
+        disabled={isCopied}
+        onClick={handleClickCopy}
+        className='absolute right-4 size-6'
+      >
+        {isCopied ? <Check /> : <Clipboard />}
+      </button>
+      {children}
+    </pre>
+  );
+}
