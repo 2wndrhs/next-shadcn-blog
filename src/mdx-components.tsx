f996c7b2df791d55c23fdec7ac4bcb8fa482e63c
2wndrhs <2wndrhs@gmail.com>
2024-09-16 11:50:57 +0900

    feat: 코드 블록에 복사 버튼 추가

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,9 @@
 import type { MDXComponents } from 'mdx/types';
+import Pre from './components/mdx/Pre';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    pre: (props) => <Pre {...props} />,
     ...components,
   };
 }
