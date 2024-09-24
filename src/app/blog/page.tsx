8d40debf557f0e1a79e84bb0f68435c275383507
2wndrhs <2wndrhs@gmail.com>
2024-09-25 01:32:37 +0900

    feat: blog 글 목록 페이지 구현

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,22 +1,51 @@
-import { getPosts } from '@/lib/posts';
+import { getPostsByYear } from '@/lib/posts';
+import dayjs from 'dayjs';
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Blog',
 };
 
 export default async function Page() {
-  const posts = await getPosts();
-  console.log(posts);
+  const posts = await getPostsByYear();
 
   return (
-    <section className=''>
-      <h2 className='mb-16 font-geistSans'>Blog</h2>
-      <div className='post-group'>
-        {posts.map(({ title }) => (
-          <div>{title}</div>
-        ))}
+    <main className='col-start-2'>
+      <h2 className='mb-16 font-geistSans font-semibold'>Blog</h2>
+      <div className='group'>
+        {posts.map(([year, postList], index) => {
+          return (
+            <div
+              key={year}
+              className={`motion-safe:animate-enter relative border-t border-border/40`}
+              style={{
+                animationDelay: `${index * 100}ms`,
+              }}
+            >
+              <h3 className='absolute top-3 text-sm text-foreground/40'>
+                {year}
+              </h3>
+              {postList.map((post) => {
+                return (
+                  <Link
+                    href={`/posts/${post.slug}`}
+                    key={post.slug}
+                    className='group/item flex transition-opacity hover:!opacity-100 group-hover:opacity-40'
+                  >
+                    <div className='ml-[20%] flex flex-1 justify-between border-t border-border/40 py-3 group-first-of-type/item:border-t-0'>
+                      <span className='font-geistSans'>{post.title}</span>
+                      <span className='px-2 text-sm text-foreground/40'>
+                        {dayjs(post.publishDate).format('MM. DD.')}
+                      </span>
+                    </div>
+                  </Link>
+                );
+              })}
+            </div>
+          );
+        })}
       </div>
-    </section>
+    </main>
   );
 }
--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -6,9 +6,7 @@ import path from 'path';
 const BASE_PATH = '/src/app/posts/(blog)';
 const POSTS_PATH = path.join(process.cwd(), BASE_PATH);
 
-export const postsPerPage = 3;
-
-export const getPosts = async (): Promise<Post[]> => {
+const readPosts = async (): Promise<Post[]> => {
   // retrieve slugs from post routes
   const slugs = (await readdir(POSTS_PATH, { withFileTypes: true })).filter(
     (dirent) => dirent.isDirectory(),
@@ -21,16 +19,32 @@ export const getPosts = async (): Promise<Post[]> => {
         `@/app/posts/(blog)/${slug}/page.mdx`
       )) as { metadata: PostMetaData };
 
-      const publishDate = dayjs(metadata.publishDate)
-        .locale('ko')
-        .format('YYYY년 MM월 DD일');
-
-      return { ...metadata, slug: slug, publishDate };
+      return { ...metadata, slug: slug };
     }),
   );
 
+  return posts;
+};
+
+export const getPosts = async (): Promise<Post[]> => {
+  const posts = await readPosts();
   // sort posts from newest to oldest
   posts.sort((a, b) => dayjs(b.publishDate).diff(dayjs(a.publishDate)));
-
   return posts;
+};
+
+export const getPostsByYear = async (): Promise<[string, Post[]][]> => {
+  const posts = await getPosts();
+  const postsByYear = new Map<string, Post[]>();
+
+  posts.forEach((post) => {
+    const year = dayjs(post.publishDate).year().toString();
+    if (!postsByYear.has(year)) {
+      postsByYear.set(year, []);
+    }
+
+    postsByYear.get(year)!.push(post);
+  });
+
+  return Array.from(postsByYear.entries());
 };
