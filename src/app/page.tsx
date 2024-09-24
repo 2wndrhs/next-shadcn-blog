0cf99f8fbcfcd938ec61a198126af988c9130ac9
2wndrhs <2wndrhs@gmail.com>
2024-09-25 01:35:09 +0900

    fix: `/` 페이지로 접근 시 `/blog`로 리다이렉트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,5 @@
-import { getPosts } from '@/lib/posts';
+import { redirect } from 'next/navigation';
 
-export default async function Home() {
-  const posts = await getPosts();
-
-  return (
-    <main>
-      <h1>Next.js MDX Blog</h1>
-      {posts.map(({ slug, title, description, publishDate, tags }) => (
-        <article key={slug}>
-          <h2>
-            <a href={`/${slug}`}>{title}</a>
-          </h2>
-          <p>{description}</p>
-          <p>
-            <strong>Published: {publishDate}</strong>
-          </p>
-          <p>
-            <strong>Tags:</strong> {tags.join(', ')}
-          </p>
-        </article>
-      ))}
-    </main>
-  );
+export default function Home() {
+  redirect('/blog');
 }
