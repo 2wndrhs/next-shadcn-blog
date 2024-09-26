eba7c1521e0d15386aafa80f0eec1fdece4e5fc1
2wndrhs <2wndrhs@gmail.com>
2024-09-27 03:58:14 +0900

    feat: `TableOfContent` 컴포넌트 구현

---
+++ src/components/TableOfContents.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+interface TOCItem {
+  id: string;
+  text: string;
+  level: number;
+}
+
+export default function TableOfContents() {
+  const [toc, setToc] = useState<TOCItem[]>([]);
+  const [activeId, setActiveId] = useState('');
+
+  useEffect(() => {
+    const headings = Array.from(document.querySelectorAll('h2, h3'));
+    console.log(headings);
+    const newToc = headings.map((heading) => ({
+      id: heading.id,
+      text: heading.textContent ?? '',
+      level: parseInt(heading.tagName.charAt(1)),
+    }));
+    setToc(newToc);
+
+    // Set up Intersection Observer
+    const observers = new Map<string, IntersectionObserver>();
+
+    const observerCallback = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log('entry', entry);
+          setActiveId(entry.target.id);
+        }
+      });
+    };
+
+    const observerOptions = {
+      rootMargin: '0px 0px -80% 0px',
+      threshold: 1.0,
+    };
+
+    headings.forEach((heading) => {
+      const element = document.getElementById(heading.id);
+      if (element) {
+        const observer = new IntersectionObserver(
+          observerCallback,
+          observerOptions,
+        );
+
+        observer.observe(element);
+        observers.set(heading.id, observer);
+      }
+    });
+
+    return () => {
+      observers.forEach((observer) => observer.disconnect());
+    };
+  }, []);
+
+  return (
+    <aside className='fixed hidden lg:block' aria-label='Table of contents'>
+      <ul className='flex flex-col gap-2 font-geistSans text-sm'>
+        {toc.map(({ id, text, level }) => (
+          <li key={id} className={`pl-${(level - 1) * 4} `}>
+            <a
+              href={`#${id}`}
+              className={`block py-0.5 transition-colors duration-200 ease-in-out hover:text-foreground/80 ${
+                activeId === id
+                  ? 'font-medium text-foreground/80'
+                  : 'text-foreground/40'
+              }`}
+            >
+              {text}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+}
