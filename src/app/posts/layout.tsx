be96d8593692ec6837a85a74b6085fee8bd6d546
2wndrhs <2wndrhs@gmail.com>
2024-09-27 04:52:05 +0900

    feat: add `Giscus` comment feature

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -1,5 +1,6 @@
-import ScrollProgressBar from '@/components/ScrollProgressBar';
-import TableOfContents from '@/components/TableOfContents';
+import GiscusComments from '@/components/posts/GiscusComments';
+import ScrollProgressBar from '@/components/posts/ScrollProgressBar';
+import TableOfContents from '@/components/posts/TableOfContents';
 
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   // Create any shared layout or styles here
@@ -9,6 +10,7 @@ export default function MdxLayout({ children }: { children: React.ReactNode }) {
       <TableOfContents />
       <main className='prose prose-slate max-w-none dark:prose-invert motion-safe:animate-enter md:col-start-2'>
         {children}
+        <GiscusComments />
       </main>
     </>
   );
