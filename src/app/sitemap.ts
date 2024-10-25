e32fc34007b184f97ff0d466b1e41da03caf8d8d
2wndrhs <2wndrhs@gmail.com>
2024-10-25 20:36:02 +0900

    feat: add blog sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import { getPosts } from '@/lib/posts';
+import type { MetadataRoute } from 'next';
+
+const defaultSitemap: MetadataRoute.Sitemap = [
+  {
+    url: 'https://joonggon.me',
+    lastModified: new Date(),
+    changeFrequency: 'daily',
+    priority: 1,
+  },
+  {
+    url: 'https://joonggon.me/blog',
+    lastModified: new Date(),
+    changeFrequency: 'weekly',
+    priority: 0.8,
+  },
+];
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await getPosts();
+
+  const sitemapFromPosts: MetadataRoute.Sitemap = posts.map((post) => ({
+    url: `https://joonggon.me/blog/${post.slug}`,
+    lastModified: post.publishDate,
+    changeFrequency: 'daily',
+    priority: 0.5,
+  }));
+
+  return [...defaultSitemap, ...sitemapFromPosts];
+}
